8baf3f037d9c8c691cf6094e93fcc82f01f41e57
Andou666 <androncoron@gmail.com>
2021-07-08 23:08:09 +0900

    commit

---
+++ source/fizzbuzz.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+for i in range(1, 101):
+    if i % 5 == 0 and i % 3 ==0:
+        print ("フィズバズ")
+    elif i % 5 == 0:
+        print ("バズ")
+    elif i % 3 == 0:
+        print ("フィズ")
+    else:
+        print (i)
